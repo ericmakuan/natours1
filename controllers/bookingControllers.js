3e9fb4ff258332fbd95d40ab0cc5e956bdd75a7b
ericmakuan <ericmakuan82502@gmail.com>
2022-04-28 22:16:26 +0800

    updated stripe3

--- controllers/bookingControllers.js
+++ controllers/bookingControllers.js
@@ -69,7 +69,7 @@ exports.getCheckoutSession = catchAsync(async(req, res, next) => {
 //   res.redirect(req.originalUrl.split('?')[0]);
 //   //多一個req至host 再跑一次這個middleware 然後沒有tour user price 直接 return next 至下一個  
 // });
-const createBookingCheckout = async session => {
+const createBookingCheckout = async (session) => {
   const tour = session.client_reference_id;
   const user = (await User.findOne({email: session.customer_email })).id;
   // const price = session.line_items[0].amount / 100;
